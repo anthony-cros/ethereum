92609e4d25d42742e7c4203fa307a6790fb6cb8a
Anthony Cros <cros.anthony@gmail.com>
2015-08-08 09:23:28 -0400

    Create olympic.js

---
+++ olympic.js
@@ -0,0 +1,12 @@
+// intentionally verbose
+function assert(condition, message) { if (!condition) { throw message || "Assertion failed"; }}
+for (blockNumber = 1029924; blockNumber <= web3.eth.getBlock("latest").number; blockNumber++) {
+  var transactions = web3.eth.getBlock(blockNumber).transactions;
+  for (transactionIndex in transactions) {
+    var transactionHash = transactions[transactionIndex];
+    var transaction = eth.getTransaction(transactionHash);
+	  assert(blockNumber == transaction.blockNumber, "ERROR: " + blockNumber + " != " + transaction.blockNumber)
+	  assert(transactionIndex == transaction.transactionIndex, "ERROR: " + transactionIndex + " != " + transaction.transactionIndex)
+    console.log(blockNumber + "\t" + transactionHash + "\t" + transaction.from + "\t" + transaction.to);
+  }
+}
